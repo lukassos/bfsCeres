7d9b29c11935fa04251e48aabbacc49b161b32b4
Lukassos <lukassos@users.noreply.github.com>
2017-01-10 01:35:17 +0100

    update urls
    updated the ulrs+d5 checksums to newest versions

--- cmake/projects.cmake
+++ cmake/projects.cmake
@@ -2,26 +2,27 @@ unset(projects)
 
 # Eigen
 list(APPEND projects eigen)
-set(eigen_version "3.2.0")
+set(eigen_version "3.3.1")
 set(eigen_url "http://bitbucket.org/eigen/eigen/get/${eigen_version}.tar.gz")
-set(eigen_md5 "9559c34af203dde5f3f1d976d859c5b3")
+set(eigen_md5 "e522cfd701168539b5c749ecd6145b65")
 
 # Metis
 list(APPEND projects metis)
-set(metis_version "5.0.1")
+set(metis_version "5.1.0")
+http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz
 set(metis_url "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-${metis_version}.tar.gz")
-set(metis_md5 "6daba5e64ed86d46e038437eda720532")
+set(metis_md5 "5465e67079419a69e0116de24fce58fe")
 
 # SuiteSparse
 list(APPEND projects SuiteSparse)
-set(SuiteSparse_version "4.2.1")
-set(SuiteSparse_url "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.2.1.tar.gz")
-set(SuiteSparse_md5 "4628df9eeae10ae5f0c486f1ac982fce")
+set(SuiteSparse_version "4.5.4")
+set(SuiteSparse_url "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-${SuiteSparse_version}.tar.gz")
+set(SuiteSparse_md5 "f6ab689442e64a1624a47aa220072d1b")
 
 # SuiteSparse and Metis for Windows
 list(APPEND projects sscmake)
 set(sscmake_version "master")
-set(sscmake_url https://github.com/jlblancoc/suitesparse-metis-for-windows/archive/master.tar.gz)
+set(sscmake_url https://github.com/lukassos/suitesparse-metis-for-windows/archive/master.tar.gz)
 set(sscmake_md5 "f7851205eca981fd78006693e0276b17")
 
 # Ceres
