6de0ff3d387939b1d8675ab85e95b31d45822143
Lukassos <lukassos@users.noreply.github.com>
2017-01-10 01:48:48 +0100

    rem patch of metis
    removed patching

--- cmake/External_SuiteSparse.cmake
+++ cmake/External_SuiteSparse.cmake
@@ -2,7 +2,7 @@ ExternalProject_Add(SuiteSparse
     DOWNLOAD_DIR ${download_dir}
     URL ${SuiteSparse_url}
     URL_MD5 ${SuiteSparse_md5}
-    PATCH_COMMAND ${MP_PATCH} -p2 -t -N < "${bfsCeres_PATCH_DIR}/metis.patch"
+    
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
